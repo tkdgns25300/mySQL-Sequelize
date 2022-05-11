b903e96ee0faa759507e78c6b85236a3265e7431
unknown <tkdgns25300@gmail.com>
2022-05-11 23:08:13 +0900

    초기 세팅(다시 해보기)

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corOptions = {
-  origin: "https://localhost:5000",
-};
+// const corOptions = {
+//   origin: "https://localhost:5000",
+// };
 
-// middleware
-app.use(cors(corOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// // middleware
+// app.use(cors(corOptions));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// api
-app.get('/', (req, res) => {
-    res.json({ message: 'hello from api' });
-})
+// // api
+// app.get('/', (req, res) => {
+//     res.json({ message: 'hello from api' });
+// })
 
-// start server
-app.listen(PORT, () => {
-  console.log(`The server is listening on port ${PORT}`);
-});
+// // start server
+// app.listen(PORT, () => {
+//   console.log(`The server is listening on port ${PORT}`);
+// });
