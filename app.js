11132da06664c35e5cb0faba6453dfe45ab66745
unknown <tkdgns25300@gmail.com>
2022-05-12 22:18:19 +0900

    초기 서버 세팅

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const corOptions = {
-//   origin: "https://localhost:5000",
-// };
+// middleware
+app.use(
+    cors({
+        origin: 'https://localhost:5000',
+        methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// // middleware
-// app.use(cors(corOptions));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+// api
+app.get('/', (req, res) => {
+    res.json({ message: "Hello from API" });
+})
 
-// // api
-// app.get('/', (req, res) => {
-//     res.json({ message: 'hello from api' });
-// })
-
-// // start server
-// app.listen(PORT, () => {
-//   console.log(`The server is listening on port ${PORT}`);
-// });
+// start server
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
