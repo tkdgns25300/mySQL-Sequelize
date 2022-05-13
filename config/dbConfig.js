3af08fc7b807052337eb25271f2d812d7ebb03da
unknown <tkdgns25300@gmail.com>
2022-05-13 23:34:24 +0900

    데이터베이스 연결 설정

---
+++ config/dbConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: 'localhost',
+    USER: '',
+    PASSWORD: '',
+    DB:'',
+    dialect: '',
+
+    pool: {
+        max: 0,
+        min: 0,
+        acquire: 0,
+        idle: 0
+    }
+}
